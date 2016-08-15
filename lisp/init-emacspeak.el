7076a1c9e17cfec0cd98681bb664843cb7766a9f
smallst <tongsmallst@gmail.com>
2016-08-15 11:18:59 +0800

    emacspeak

--- lisp/init-emacspeak.el
+++ lisp/init-emacspeak.el
@@ -2,6 +2,7 @@
 ; add "export DTK_PROGRAM=espeak" in my ~/.bashrc
 (defun emspk ()
   (interactive)
-  (load-file "/usr/share/emacs/site-lisp/emacspeak/lisp/emacspeak-setup.el")
+                                        ;(load-file "/usr/share/emacs/site-lisp/emacspeak/lisp/emacspeak-setup.el")
+  (load-file "/home/smallst/Test/emacspeak/lisp/emacspeak-setup.el")
   )
 (provide 'init-emacspeak)
