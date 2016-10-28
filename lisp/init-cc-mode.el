4cd7d300f281e48e3a5538945d54aebcbb1fc066
smallst <tongsmallst@gmail.com>
2016-10-28 10:10:38 +0800

    indent | c substatement

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -32,7 +32,7 @@
   ;; google "C/C++/Java code indentation in Emacs" for more advanced skills
   ;; C code:
   ;;   if(1) // press ENTER here, zero means no indentation
-  (fix-c-indent-offset-according-to-syntax-context 'substatement 0)
+  ;(fix-c-indent-offset-according-to-syntax-context 'substatement 0)
   ;;   void fn() // press ENTER here, zero means no indentation
   (fix-c-indent-offset-according-to-syntax-context 'func-decl-cont 0))
 
